ae5860da9401f23bd1b43a69f4e112aad7d9a53b
Michael Leblanc <mleblanc1@werfen.com>
2023-11-30 18:40:32 -0500

    made some changes to header component

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -3,8 +3,8 @@ import 'tachyons';
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function Header() {
     return (
-      <ul className="flex pl0">
-          <li className="ma5 pl0">Michael LeBlanc</li>
+      <ul className="flex list pl0">
+          <li className="ma5 p8">Michael LeBlanc</li>
           <li className="ma5">About Me</li>
           <li className="ma5">Portfolio</li>
           <li className="ma5">Contact</li>
