7db30284d3a5b6300a3816853bc474dd15e83f0d
Michael Leblanc <mleblanc1@werfen.com>
2023-12-02 14:20:55 -0500

    added some of the final changes

--- react-portfolio/src/pages/PortfolioPage.jsx
+++ react-portfolio/src/pages/PortfolioPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Project from '../components/Project'; // Update the path accordingly
+import bookreviewPic from '../assets/bookreviewScreenshot.png';
+import restaurantFinderPic from '../assets/restaurantFinderScreenshot.png';
+import weatherFinderPic from '../assets/weatherScreenshot.png';
+import pwaPic from '../assets/pwaScreenshot.png';
+import jsquizPic from '../assets/jsquizScreenshot.png';
+import patriotsPic from '../assets/patriotsScreenshot.png';
+import githubLogoPic from '../assets/githubLogo.png';
 
 export default function PortfolioPage() {
   // Sample data for projects
   const projects = [
-    { title: 'Book Review', technologies: 'Handlebars, Node.js, MySql, Express.js', linkToProject: 'https://book-reviewer-unh-13a415d3c295.herokuapp.com/', githubRepo:'https://github.com/mleblanc94/book-reviewer' },
-    { title: 'Quality Restaurant Finder', technologies: 'HTML, CSS, Javascript, Rest APIs', linkToProject: 'https://mleblanc94.github.io/quality-restaurant-finder/', githubRepo:'https://github.com/mleblanc94/quality-restaurant-finder'},
-    { title: 'Weather Finder', technologies: 'HTML, CSS, Javascript, Rest APIs', linkToProject: 'https://mleblanc94.github.io/weather-dashboard/', githubRepo: 'https://github.com/mleblanc94/weather-dashboard'},
-    { title: 'Progressive Web Application Text Editor', technologies: 'Lighthouse, Webpack, IndexDB API', linkToProject: 'https://text-editor-pwa-l6dx.onrender.com/', githubRepo: 'https://github.com/mleblanc94/text-editor-pwa'},
-    { title: 'Javascript Quiz', technologies: 'HTML, CSS, Javascript', linkToProject: 'https://mleblanc94.github.io/javascript-quiz/', githubRepo: 'https://github.com/mleblanc94/javascript-quiz'},
-    { title: 'Patriots/Mario Game', technologies: 'Kaboom.js', linkToProject: 'https://mleblanc94.github.io/My-Patriots-Game/', githubRepo:'https://github.com/mleblanc94/My-Patriots-Game'},
+    { title: 'Book Review', technologies: 'Handlebars, Node.js, MySql, Express.js', linkToProject: 'https://book-reviewer-unh-13a415d3c295.herokuapp.com/', githubRepo:'https://github.com/mleblanc94/book-reviewer', screenshot: bookreviewPic, githubLogo: githubLogoPic },
+    { title: 'Quality Restaurant Finder', technologies: 'HTML, CSS, Javascript, Rest APIs', linkToProject: 'https://mleblanc94.github.io/quality-restaurant-finder/', githubRepo:'https://github.com/mleblanc94/quality-restaurant-finder', screenshot: restaurantFinderPic, githubLogo: githubLogoPic},
+    { title: 'Weather Finder', technologies: 'HTML, CSS, Javascript, Rest APIs', linkToProject: 'https://mleblanc94.github.io/weather-dashboard/', githubRepo: 'https://github.com/mleblanc94/weather-dashboard', screenshot: weatherFinderPic, githubLogo: githubLogoPic},
+    { title: 'Progressive Web Application Text Editor', technologies: 'Lighthouse, Webpack, IndexDB API', linkToProject: 'https://text-editor-pwa-l6dx.onrender.com/', githubRepo: 'https://github.com/mleblanc94/text-editor-pwa', screenshot: pwaPic, githubLogo: githubLogoPic},
+    { title: 'Javascript Quiz', technologies: 'HTML, CSS, Javascript', linkToProject: 'https://mleblanc94.github.io/javascript-quiz/', githubRepo: 'https://github.com/mleblanc94/javascript-quiz', screenshot: jsquizPic, githubLogo: githubLogoPic},
+    { title: 'Patriots/Mario Game', technologies: 'Kaboom.js', linkToProject: 'https://mleblanc94.github.io/My-Patriots-Game/', githubRepo:'https://github.com/mleblanc94/My-Patriots-Game', screenshot: patriotsPic, githubLogo: githubLogoPic},
   ];
 
   return (
@@ -22,6 +29,7 @@ export default function PortfolioPage() {
             technologies={project.technologies}
             linkToProject={project.linkToProject}
             githubRepo={project.githubRepo}
+            screenshot={project.screenshot}
           />
         ))}
       </div>
